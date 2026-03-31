7b86f19b2a5b2b391129a59d007c7a72a9d28125
Tony <antony.branquinho@gmail.com>
2026-03-31 20:09:27 -0300

    reenviando o commit

--- src/main/java/com/empresafake/service/IdeiaService.java
+++ src/main/java/com/empresafake/service/IdeiaService.java
@@ -1,5 +1,32 @@
-package main.java.com.empresafake.service;
+package com.empresafake.service;
+
+import com.empresafake.model.Ideia;
+import java.util.ArrayList;
+import java.util.List;
 
 public class IdeiaService {
 
+    private List<Ideia> ideias = new ArrayList<>();
+    private Long proximoId = 1L;
+
+    public Ideia adicionarIdeia(String titulo, String descricao) {
+        if (titulo == null || titulo.isEmpty()) {
+            throw new IllegalArgumentException("O título não pode ser nulo ou vazio.");
+        }
+
+        
+        String status = "NORMAL";
+        if (descricao != null && descricao.toLowerCase().contains("urgente")) {
+            status = "ALTA PRIORIDADE";
+        }
+        
+        
+        Ideia ideia = new Ideia(proximoId++, titulo, descricao, "PENDENTE");
+        ideias.add(ideia);
+        return ideia;
+    }
+
+    public List<Ideia> listarIdeias() {
+        return ideias;
+    }
 }
