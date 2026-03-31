c8617b04e475b90ff61d031a875e5eb9af4e37da
Tony <antony.branquinho@gmail.com>
2026-03-31 19:22:20 -0300

    editei a RelatorioService

--- src/main/java/com/empresafake/service/RelatorioService.java
+++ src/main/java/com/empresafake/service/RelatorioService.java
@@ -1,10 +1,19 @@
 package com.empresafake.service;
 
+import java.util.Objects;
+
 public class RelatorioService {
 
      public void gerarRelatorio(List<Ideia> ideiasCadastradas) {
+    	 Objects.requireNonNull(ideiasCadastradas, "A lista fornecida é nula");
+    	 
          for (Ideia ideia : ideiasCadastradas) {
-             String nomeAutor = ideia.getAutor().getNome();
+        	 String nomeAutor = "Autor não informado";
+             
+             if (ideia.getAutor() != null) {
+            	 nomeAutor = ideia.getAutor().getNome();
+             }
+             
              System.out.println("Ideia: " + ideia.getTitulo() + " | Autor: " + nomeAutor);
          }
      }
