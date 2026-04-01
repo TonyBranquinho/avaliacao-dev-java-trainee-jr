cb15a7b7f3de785b2164bfd7017dc116b45a0bd8
Tony <antony.branquinho@gmail.com>
2026-04-01 12:09:33 -0300

    Reestrutura projeto para Spring Boot mantendo classes das issues anteriores

---
+++ src/main/java/com/empresafake/incubadora/IncubadoraApplication.java
@@ -0,0 +1,13 @@
+package com.empresafake.incubadora;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IncubadoraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IncubadoraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/empresafake/incubadora/IncubadoraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.empresafake.incubadora;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IncubadoraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
